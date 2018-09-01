563ecaa4b42f6f2073da8dec3e980284b044bf45
Alex Collins <alex_collins@intuit.com>
2018-09-01 11:45:46 +0100

    logback

---
+++ src/main/java/bt/Args.java
@@ -0,0 +1,35 @@
+package bt;
+
+import lombok.Value;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Value
+public class Args {
+
+    private final Map<String, Object> options = new HashMap<>();
+    private final List<String> commands = new ArrayList<>();
+
+    private Args(String[] args) {
+        String optionName = null;
+        for (String arg : args) {
+            if (arg.startsWith("--")) {
+                optionName = arg.substring(2);
+            } else if (arg.startsWith("-")) {
+                optionName = arg.substring(1);
+            } else if (optionName != null) {
+                options.put(optionName, arg);
+                optionName = null;
+            } else {
+                commands.add(arg);
+            }
+        }
+    }
+
+    public static Args parse(String[] args) {
+        return new Args(args);
+    }
+}
--- src/main/java/bt/Main.java
+++ src/main/java/bt/Main.java
@@ -1,8 +1,17 @@
 package bt;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
+        LOGGER.info("starting");
+        try {
 
+        } finally {
+            LOGGER.info("done");
+        }
     }
 }
