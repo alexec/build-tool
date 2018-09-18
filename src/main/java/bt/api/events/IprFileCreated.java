6ee2d15022210eac62f0d87578c3870c356b44c3
Alex Collins <alex_collins@intuit.com>
2018-09-18 12:50:41 +0100

    ipr

---
+++ src/main/java/bt/api/events/IprFileCreated.java
@@ -0,0 +1,18 @@
+package bt.api.events;
+
+import java.nio.file.Path;
+
+public class IprFileCreated {
+    private final Path output;
+
+    public IprFileCreated(Path output) {
+        this.output = output;
+    }
+
+    @Override
+    public String toString() {
+        return "IprFileCreated(" +
+                output +
+                ')';
+    }
+}
